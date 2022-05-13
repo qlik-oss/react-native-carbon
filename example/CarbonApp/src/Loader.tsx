61bb6c4c0933646dccb0815d3e0a8eddcb9107ae
Vittorio Cellucci <vel@qlik.com>
2022-05-12 20:26:45 -0400

    fix: replace CarbonApp with simpler Galaxy

--- example/CarbonApp/src/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {ActivityIndicator} from 'react-native-paper';
-
-const Loader = () => {
-  // eslint-disable-next-line react-native/no-inline-styles
-  return <ActivityIndicator size="large" style={{flex: 1}} />;
-};
-
-export default Loader;
